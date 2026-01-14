d998dab21d7590e19a352b602cd5d7dee4fa1f49
Kimmingki <a01050919568@gmail.com>
2026-01-14 17:24:31 +0900

    enum 파일 생성

---
+++ src/main/java/com/example/shop/domain/enums/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.example.shop.domain.enums;
+
+public enum OrderStatus {
+
+    CREATED, PAID, PREPARING, SHIPPED, DELIVERED, CANCELED
+}
---
+++ src/main/java/com/example/shop/domain/enums/ProductStatus.java
@@ -0,0 +1,6 @@
+package com.example.shop.domain.enums;
+
+public enum ProductStatus {
+
+    ON_SALE, SOLD_OUT, HIDDEN
+}
